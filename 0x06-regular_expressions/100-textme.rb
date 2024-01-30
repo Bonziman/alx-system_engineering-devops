0895e9e7be4040d3d565ac548763f6d13858b361
Bonziman <eloirdiwiggmail.com>
2024-01-30 09:08:45 -0800

    textme here i come

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+string = ARGV[0]
+sender_pattern = /\[from:([^\]]+)\]/
+reciever_pattern = /\[to:([^\]]+)\]/
+flags_pattern = /\[flags:([^\]]*)\]/
+
+sender = string.match(sender_pattern)
+reciever = string.match(reciever_pattern)
+flags = string.match(flags_pattern)
+
+puts "#{sender[1]}, #{reciever[1]}, #{flags[1]}"
