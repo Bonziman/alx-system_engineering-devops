dd9a02b0ba82c9cae52af2394092068161e3dd0b
Bonziman <eloirdiwi@gmail.com>
2024-03-23 22:36:27 +0000

    task 2 redoooo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # install flask from pip3
-package { 'flask':
- insure => 2.1.0 ,
- provider => 'pip3',	
+package { 'pip3':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['pip3'],
 }
