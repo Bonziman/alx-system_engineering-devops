b081d26d54800abc95a5fb202bccc20c25317ac5
Bonziman <eloirdiwi@gmail.com>
2024-05-20 22:26:57 +0100

    export to CSV fixed pycodestyle

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,34 +5,38 @@ import json
 import requests
 import csv
 
+
 def main():
-        usr_id = argv[1]
-        user_link = f'https://jsonplaceholder.typicode.com/users/{usr_id}'
-        todos_link = f'https://jsonplaceholder.typicode.com/todos?userId={usr_id}'
+    usr_id = argv[1]
+    user_link = f'https://jsonplaceholder.typicode.com/users/{usr_id}'
+    todos_link = (f'https://jsonplaceholder.typicode.com/\
+                     todos?userId={usr_id}')
 
-        try:
-            user_res = requests.get(user_link)
-            todos_res = requests.get(todos_link)
+    try:
+        user_res = requests.get(user_link)
+        todos_res = requests.get(todos_link)
 
-            if user_res.status_code != 200 or todos_res.status_code != 200:
-                print("Error fetching data from API")
-                return
+        if user_res.status_code != 200 or todos_res.status_code != 200:
+            print("Error fetching data from API")
+            return
 
-            user_data = user_res.json()
-            todos_data = todos_res.json()
+        user_data = user_res.json()
+        todos_data = todos_res.json()
 
-            user_name = user_data['username']
+        user_name = user_data['username']
 
-            csv_file = f"{usr_id}.csv"
-            print(usr_id)
-            with open('{}.csv'.format(usr_id), 'w') as csvfile:
-                writer = csv.writer(csvfile, delimiter=',',
-                quotechar='"', quoting=csv.QUOTE_ALL)
-                for todo in todos_data:
-                    writer.writerow([todo['userId'], user_data['username'],
-                                    todo['completed'], todo['title']])
-        except requests.exceptions.RequestException as e:
-            print(f"Request failed: {e}")
+        csv_file = f"{usr_id}.csv"
+        print(usr_id)
+        with open('{}.csv'.format(usr_id), 'w') as csvfile:
+            writer = csv.writer(csvfile,
+                                delimiter=',',
+                                quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+            for todo in todos_data:
+                writer.writerow([todo['userId'], user_data['username'],
+                                todo['completed'], todo['title']])
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
 
 
 if __name__ == "__main__":
