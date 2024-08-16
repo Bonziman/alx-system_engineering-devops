e3454a1f26dd097b27255973fe083d207da42582
Bonziman <eloirdiwi@gmail.com>
2024-08-16 20:41:28 +0100

    trace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# This puppet script fixes the Word-Press site
-exec { 'wp_fixer':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# automated puppet fix (to find out why Apache is returning a 500 error)
+
+exec { 'Fix wordpress site':
+	command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+	provider => shell,
 }
